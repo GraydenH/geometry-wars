8fa77e0877d0e713695705e13a15a685d20ab2c0
Grayden Hormes <graydenhormes@gmail.com>
2021-06-01 22:36:01 -0700

    finish tutorial 6

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate glium;
 
+use std::io::Cursor;
+
 fn main() {
     #[allow(unused_imports)]
     use glium::{glutin, Surface};
@@ -10,47 +12,53 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
+    let image = image::load(Cursor::new(&include_bytes!("opengl.png")),
+                            image::ImageFormat::Png).unwrap().to_rgba8();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
+
     #[derive(Copy, Clone)]
     struct Vertex {
         position: [f32; 2],
+        tex_coords: [f32; 2],
     }
 
-    implement_vertex!(Vertex, position);
+    implement_vertex!(Vertex, position, tex_coords);
 
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25], tex_coords: [1.0, 0.0] };
     let shape = vec![vertex1, vertex2, vertex3];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
-    let vertex_shader_src = r#"#version 140
+    let vertex_shader_src = r#"
+        #version 140
         in vec2 position;
-        out vec2 my_attr;      // our new attribute
-
+        in vec2 tex_coords;
+        out vec2 v_tex_coords;
         uniform mat4 matrix;
-
         void main() {
-            my_attr = position;     // we need to set the value of each `out` variable.
+            v_tex_coords = tex_coords;
             gl_Position = matrix * vec4(position, 0.0, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 140
-
-        in vec2 my_attr;
+        in vec2 v_tex_coords;
         out vec4 color;
-
+        uniform sampler2D tex;
         void main() {
-            color = vec4(my_attr, 0.0, 1.0);   // we build a vec4 from a vec2 and two floats
+            color = texture(tex, v_tex_coords);
         }
     "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    let mut t: f32 = -0.5;
+    let mut t = -0.5;
     event_loop.run(move |event, _, control_flow| {
 
         match event {
@@ -74,20 +82,22 @@ fn main() {
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         // we update `t`
-        t += 0.002;
+        t += 0.0002;
         if t > 0.5 {
             t = -0.5;
         }
 
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
+
         let uniforms = uniform! {
             matrix: [
-                [ t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
                 [0.0, 0.0, 1.0, 0.0],
-                [t, 0.0, 0.0, 1.0f32],
-            ]
+                [ t , 0.0, 0.0, 1.0f32],
+            ],
+            tex: &texture,
         };
 
         target.draw(&vertex_buffer, &indices, &program, &uniforms,
