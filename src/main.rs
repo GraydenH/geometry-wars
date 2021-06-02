04690adb04c348ae31c2fceacccd8b6879007c22
Grayden Hormes <graydenhormes@gmail.com>
2021-06-02 09:23:27 -0700

    finish tutortial 10

--- src/main.rs
+++ src/main.rs
@@ -25,37 +25,40 @@ fn main() {
     let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,
                                           &teapot::INDICES).unwrap();
     let vertex_shader_src = r#"
-        #version 150      // updated
+        #version 140
 
         in vec3 position;
         in vec3 normal;
 
-        out vec3 v_normal;      // new
+        out vec3 v_normal;
 
+        uniform mat4 perspective;       // new
         uniform mat4 matrix;
 
         void main() {
-            v_normal = transpose(inverse(mat3(matrix))) * normal;       // new
-            gl_Position = matrix * vec4(position, 1.0);
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
+            gl_Position = perspective * matrix * vec4(position, 1.0);       // new
         }
     "#;
 
     let fragment_shader_src = r#"
-#version 140
+        #version 140
 
-in vec3 v_normal;
-out vec4 color;
-uniform vec3 u_light;
+        in vec3 v_normal;
+        out vec4 color;
+        uniform vec3 u_light;
 
-void main() {
-    float brightness = dot(normalize(v_normal), normalize(u_light));
-    vec3 dark_color = vec3(0.6, 0.0, 0.0);
-    vec3 regular_color = vec3(1.0, 0.0, 0.0);
-    color = vec4(mix(dark_color, regular_color, brightness), 1.0);
-}
+        void main() {
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+        }
     "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
 
     let mut t = -0.5;
     event_loop.run(move |event, _, control_flow| {
@@ -87,20 +90,48 @@ void main() {
         }
 
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [
+                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+                [         0.0         ,     f ,              0.0              ,   0.0],
+                [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
+                [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
+            ]
+        };
 
         let matrix = [
             [0.01, 0.0, 0.0, 0.0],
             [0.0, 0.01, 0.0, 0.0],
             [0.0, 0.0, 0.01, 0.0],
-            [0.0, 0.0, 0.0, 1.0f32]
+            [0.0, 0.0, 2.0, 1.0f32]
         ];
-// the direction of the light
+
+        // the direction of the light
         let light = [-1.0, 0.4, 0.9f32];
 
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::draw_parameters::DepthTest::IfLess,
+                write: true,
+                .. Default::default()
+            },
+            .. Default::default()
+        };
+
         target.draw((&positions, &normals), &indices, &program,
-                    &uniform! { matrix: matrix, u_light: light },
-                    &Default::default()).unwrap();
+                    &uniform! { matrix: matrix, perspective: perspective, u_light: light },
+                    &params).unwrap();
         target.finish().unwrap();
     });
 }
