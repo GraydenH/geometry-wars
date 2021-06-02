7fa0a9f730213d1b730ffa66c7d5d80e20d78aa3
Grayden Hormes <graydenhormes@gmail.com>
2021-06-01 21:10:24 -0700

    finish tutorial 5
    https://github.com/glium/glium/blob/master/book/tuto-05-colors.md

--- src/main.rs
+++ src/main.rs
@@ -25,23 +25,26 @@ fn main() {
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
-    let vertex_shader_src = r#"
-        #version 140
-
+    let vertex_shader_src = r#"#version 140
         in vec2 position;
+        out vec2 my_attr;      // our new attribute
 
         uniform mat4 matrix;
 
         void main() {
+            my_attr = position;     // we need to set the value of each `out` variable.
             gl_Position = matrix * vec4(position, 0.0, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 140
+
+        in vec2 my_attr;
         out vec4 color;
+
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            color = vec4(my_attr, 0.0, 1.0);   // we build a vec4 from a vec2 and two floats
         }
     "#;
 
