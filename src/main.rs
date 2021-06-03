9ff6c1a041e5ed5909bec05c1d932c38bccf42d0
Grayden Hormes <graydenhormes@gmail.com>
2021-06-02 22:42:28 -0700

    finish tutorial 11

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #[macro_use]
 extern crate glium;
 
-use std::io::Cursor;
-
+#[path = "teapot.rs"]
 mod teapot;
 
 fn main() {
@@ -11,43 +10,34 @@ fn main() {
 
     let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
+    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
-
-    let image = image::load(Cursor::new(&include_bytes!("opengl.png")),
-                            image::ImageFormat::Png).unwrap().to_rgba8();
-    let image_dimensions = image.dimensions();
-    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
 
     let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
     let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
     let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,
                                           &teapot::INDICES).unwrap();
-    let vertex_shader_src = r#"
-        #version 140
 
+    let vertex_shader_src = r#"
+        #version 150
         in vec3 position;
         in vec3 normal;
-
         out vec3 v_normal;
-
-        uniform mat4 perspective;       // new
-        uniform mat4 matrix;
-
+        uniform mat4 perspective;
+        uniform mat4 view;
+        uniform mat4 model;
         void main() {
-            v_normal = transpose(inverse(mat3(matrix))) * normal;
-            gl_Position = perspective * matrix * vec4(position, 1.0);       // new
+            mat4 modelview = view * model;
+            v_normal = transpose(inverse(mat3(modelview))) * normal;
+            gl_Position = perspective * modelview * vec4(position, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
-        #version 140
-
+        #version 150
         in vec3 v_normal;
         out vec4 color;
         uniform vec3 u_light;
-
         void main() {
             float brightness = dot(normalize(v_normal), normalize(u_light));
             vec3 dark_color = vec3(0.6, 0.0, 0.0);
@@ -56,12 +46,13 @@ fn main() {
         }
     "#;
 
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src,
+                                              None).unwrap();
 
-    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
-
-    let mut t = -0.5;
     event_loop.run(move |event, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
@@ -79,18 +70,17 @@ fn main() {
             _ => return,
         }
 
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-
-        // we update `t`
-        t += 0.0002;
-        if t > 0.5 {
-            t = -0.5;
-        }
-
         let mut target = display.draw();
         target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+
+        let model = [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 2.0, 1.0f32]
+        ];
+
+        let view = view_matrix(&[1.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
 
         let perspective = {
             let (width, height) = target.get_dimensions();
@@ -110,14 +100,6 @@ fn main() {
             ]
         };
 
-        let matrix = [
-            [0.01, 0.0, 0.0, 0.0],
-            [0.0, 0.01, 0.0, 0.0],
-            [0.0, 0.0, 0.01, 0.0],
-            [0.0, 0.0, 2.0, 1.0f32]
-        ];
-
-        // the direction of the light
         let light = [-1.0, 0.4, 0.9f32];
 
         let params = glium::DrawParameters {
@@ -126,12 +108,48 @@ fn main() {
                 write: true,
                 .. Default::default()
             },
+            //backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
             .. Default::default()
         };
 
         target.draw((&positions, &normals), &indices, &program,
-                    &uniform! { matrix: matrix, perspective: perspective, u_light: light },
+                    &uniform! { model: model, view: view, perspective: perspective, u_light: light },
                     &params).unwrap();
         target.finish().unwrap();
     });
+}
+
+
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [up[1] * f[2] - up[2] * f[1],
+        up[2] * f[0] - up[0] * f[2],
+        up[0] * f[1] - up[1] * f[0]];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+        f[2] * s_norm[0] - f[0] * s_norm[2],
+        f[0] * s_norm[1] - f[1] * s_norm[0]];
+
+    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
 }
